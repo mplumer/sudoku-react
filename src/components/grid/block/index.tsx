08a567eda23658c68270278163af339a510b7a9b
Max Plumer <maxplumer12@gmail.com>
2021-06-07 14:17:56 -0400

    render grid numbers

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -8,7 +8,7 @@ interface IProps {
 }
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`} />
+  return <Container data-cy={`block-${rowIndex}-${colIndex}`}>0</Container>
 }
 
 export default Block
