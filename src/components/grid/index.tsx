1fe9848ddc0504733edc3924f80a63ef70cc99e8
Max Plumer <maxplumer12@gmail.com>
2021-06-16 13:35:23 -0400

    ADD: mousetrap for arrow selector functions

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,18 +1,72 @@
 import React, { FC, Children, useCallback, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import useMousetrap from 'react-hook-mousetrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { AnyAction, Dispatch } from 'redux'
 
-import { createGrid } from 'reducers'
+import { createGrid, IReducer, selectBlock } from 'reducers'
+import { INDEX, BLOCK_COORDS } from 'typings'
 
 import Block from './block'
 import { Container, Row } from './styles'
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS
+}
+
 const Grid: FC = () => {
+  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
+    selectedBlock,
+  }))
   const dispatch = useDispatch<Dispatch<AnyAction>>()
   const create = useCallback(() => dispatch(createGrid()), [dispatch])
   useEffect(() => {
     create()
   }, [create])
+
+  function moveDown() {
+    if (state.selectedBlock && state.selectedBlock[0] < 8)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] + 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+  }
+
+  function moveLeft() {
+    if (state.selectedBlock && state.selectedBlock[1] > 0)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] - 1) as INDEX,
+        ])
+      )
+  }
+
+  function moveRight() {
+    if (state.selectedBlock && state.selectedBlock[1] < 8)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] + 1) as INDEX,
+        ])
+      )
+  }
+
+  function moveUp() {
+    if (state.selectedBlock && state.selectedBlock[0] > 0)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] - 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+  }
+
+  useMousetrap('down', moveDown)
+  useMousetrap('left', moveLeft)
+  useMousetrap('right', moveRight)
+  useMousetrap('up', moveUp)
 
   return (
     <Container data-cy="grid-container">
@@ -21,7 +75,10 @@ const Grid: FC = () => {
           <Row data-cy="grid-row-container">
             {Children.toArray(
               [...Array(9)].map((_, colIndex) => (
-                <Block colIndex={colIndex} rowIndex={rowIndex} />
+                <Block
+                  colIndex={colIndex as INDEX}
+                  rowIndex={rowIndex as INDEX}
+                />
               ))
             )}
           </Row>
